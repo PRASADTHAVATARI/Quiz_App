26d436e622ba17ae193ba5939854e7841d8b71f3
tavatari prasad <tprasad191201@gmail.com>
2025-01-08 23:16:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Quizapp from './Quizaapp'
+
+export default function App() {
+  return (
+    <div>
+      <Quizapp></Quizapp>
+    </div>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const data = [
+    {
+        Question: "What does HTML stand for?",
+        Option1: "Hyper Text Markup Language",
+        Option2: "Hyperlink Text Markup Language",
+        Option3: "High-level Text Machine Language",
+        Option4: "Hyper Transfer Markup Language"
+    },
+    {
+        Question: "What is the purpose of the `<head>` tag in HTML?",
+        Option1: "To contain metadata and links to resources",
+        Option2: "To define the main content of the page",
+        Option3: "To display headings",
+        Option4: "To link external files"
+    },
+    {
+        Question: "Which CSS property is used to change the background color?",
+        Option1: "background-color",
+        Option2: "color",
+        Option3: "bg-color",
+        Option4: "background-style"
+    },
+    {
+    
+        Question: "What does CSS stand for?",
+        Option1: "Creative Style System",
+        Option2: "Computer Styling Syntax",
+        Option3: "Code Styling Sheets",
+        Option4: "Cascading Style Sheets",
+    },
+    {
+    
+        Question: "What is the use of the `alt` attribute in the `<img>` tag?",
+        Option1: "To provide alternative text for the image",
+        Option2: "To set the alignment of the image",
+        Option3: "To specify the type of the image",
+        Option4: "To link the image to another file"
+    },
+    {
+    
+        Question: "Which HTML tag is used to create a hyperlink?",
+        Option1: "<a>",
+        Option2: "<link>",
+        Option3: "<href>",
+        Option4: "<url>"
+    },
+    {
+    
+        Question: "Which method is used to add an element to the end of an array in JavaScript?",
+        Option1: "push()",
+        Option2: "pop()",
+        Option3: "append()",
+        Option4: "insert()"
+    },
+    {
+    
+        Question: "How can you include an external JavaScript file in an HTML document?",
+        Option1: "Using the `<script src='filename.js'>` tag",
+        Option2: "Using the `<link src='filename.js'>` tag",
+        Option3: "Using the `<javascript src='filename.js'>` tag",
+        Option4: "Using the `<file src='filename.js'>` tag"
+    },
+    {
+    
+        Question: "Which symbol is used for single-line comments in JavaScript?",
+        Option1: "//",
+        Option2: "/*",
+        Option3: "#",
+        Option4: "<!--"
+    },
+    {
+        Question: "What is the purpose of the `flexbox` layout in CSS?",
+        Option1: "To arrange elements in a flexible and responsive manner",
+        Option2: "To create static grids",
+        Option3: "To define fixed sizes for all elements",
+        Option4: "To overlap elements on the webpage"
+    }
+];
